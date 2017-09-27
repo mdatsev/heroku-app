e4d8300963a709d8e7a9b77d6301757bdbe42338
mdatsev <martin.datsev.p@gmail.com>
2017-09-27 02:23:04 -0700

    quadratic equation now

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
   			def index
-    			if(params.has_key?('a') && params.has_key?('b'))
-      				@sum = params['a'].to_i + params['b'].to_i
+    			if(params.has_key?('a'))
+  					a = params['a'].to_i;
+  					b = params['b'].to_i;
+  					c = params['c'].to_i;
+  					d = b * b - 4*a*c;
+      				@x1 = (-b + Math.sqrt(d))/(2*a)
+      				@x2 = (-b - Math.sqrt(d))/(2*a)
     			end
   			end
 end
