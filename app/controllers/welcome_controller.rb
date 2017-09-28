4a7c1b79af6f71af4df5610a39cd29dfd1f273f5
mdatsev <martin.datsev.p@gmail.com>
2017-09-28 07:21:55 -0700

    csv stuff.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,11 @@
 class WelcomeController < ApplicationController
-  			def index
-    			if(params.has_key?('a'))
-  					a = params['a'].to_i;
-  					b = params['b'].to_i;
-  					c = params['c'].to_i;
-  					d = b * b - 4*a*c;
-      				@x1 = (-b + Math.sqrt(d))/(2*a)
-      				@x2 = (-b - Math.sqrt(d))/(2*a)
-    			end
-  			end
+  def index
+  
+  end
+
+
+  def upload
+    recieved_file = params[:file].read;
+    @file = recieved_file;
+  end
 end
